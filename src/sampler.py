00f48fb33987aa16fa68cdfbc8d03fa88fec4ce8
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-23 21:09:21 +0900

    succeeded in downloading Youtube

--- src/sampler.py
+++ src/sampler.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
+from pytube import YouTube
 import sys
-import subprocess
-
 
 options = [x for x in sys.argv if x.startswith("-")]
 
-def syscall(cmds, error_message=''):
-    try:
-        output = subprocess.check_output(cmds.split(' '))
-    except:
-        print("\n%s\n" % error_message)
-        sys.exit()
+def help():
+    print("")
+    print("sampler --download ... download .wav file from youtube")
+    print("")
 
-syscall("which youtube-dl", "Command does not exist. You need to install youtube-dl")
-
-
+if ("-h" in options) or ("help" in sys.argv):
+    help()
+elif("--download" in options):
+    urls = [x for x in sys.argv if x.startswith("https://www.youtube.com/watch")]
+    for url in urls:
+        print("now downloading %s...." % url)
+        YouTube(url)     \
+            .streams     \
+            .first()     \
+            .download()
