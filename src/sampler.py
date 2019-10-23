4cdadf5821cb151c06aa2895559a6412eec36d13
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-23 21:23:26 +0900

    succeeded in converting mp4 into wav

--- src/sampler.py
+++ src/sampler.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
 from pytube import YouTube
+import os
+import subprocess
 import sys
 
 options = [x for x in sys.argv if x.startswith("-")]
 
 def help():
     print("")
-    print("sampler --download ... download .wav file from youtube")
+    print("sampler --wav ... convert file to youtube")
     print("")
 
-if ("-h" in options) or ("help" in sys.argv):
-    help()
-elif("--download" in options):
+def mp4_to_wav(video_name):
+    wav_ext = video_name.replace(".mp4", "")
+    command = "ffmpeg -i %s -ab 160k -ac 2 -ar 44100 -vn %s.wav" % (video_name, wav_ext)
+    subprocess.call(command, shell=True)
+
+def download():
     urls = [x for x in sys.argv if x.startswith("https://www.youtube.com/watch")]
     for url in urls:
         print("now downloading %s...." % url)
@@ -20,3 +25,18 @@ elif("--download" in options):
             .streams     \
             .first()     \
             .download()
+
+def pwd():
+    os.chdir(os.path.dirname(__file__))
+    return os.getcwd()
+
+if ("-h" in options) or ("help" in sys.argv):
+    help()
+elif("--wav" in options):
+    download  ()
+    for r, d, f in os.walk("."):
+        for file in f:
+            if '.mp4' in file:
+                mp4_to_wav(os.path.join(r, file))
+else:
+    help()
